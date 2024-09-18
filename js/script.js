6fbfc208ccb6416b2336ffd30d76a9d9559803a2
Md. Khairul Hasan <darknessmoon76@gmail.com>
2024-09-18 12:49:43 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,117 @@
+// ..............................................................................
+// function calculation() {
+//   console.log("here will be + -");
+// }
+// calculation()
+// ..............................................................................
+
+// function calculation() {
+  //   let x = 500
+  //   let y = 300
+  //   let z = x + y
+  //   let result = z
+  //   console.log(result);
+
+  // }
+  // calculation()
+// ..............................................................................
+// function jogBiog() {
+//   let x = 500
+//   let y = 300
+//   let z = x + y
+//   let result = z
+//   console.log(result);
+// }
+// jogBiog()
+// ..............................................................................
+// let team = ["A", "B", "C", "D", "E", "F", "G", "H", "i", "j"]
+// // console.log(teams);
+// // console.log(teams.length);
+// for (let i = 0; i < team.length; i++) {
+//   console.log(team[i]);
+// }
+// ..............................................................................
+// let team = ["A", "B", "C", "D", "E", "F", "G", "H", "i", "j"]
+// // console.log(teams);
+// // console.log(teams.length);
+// for (let i = 0; i < team.length; i++) {
+//   console.log(team[i-6]);
+// }
+// ..............................................................................
+// let team = ["A", "B", "C", "D", "E", "F", "G", "H", "i", "j"]
+// team.map((item) => {
+//   console.log(item);
+// })
+// ..............................................................................
+// let team = ["A", "B", "C", "D", "E", "F", "G", "H", "i", "j"]
+// team.map((JBG) => {
+//   console.log(JBG);
+// })
+// ..............................................................................forEach heavy element
+// let team = ["A", "B", "C", "D", "E", "F", "G", "H", "i", "j"]
+// team.forEach((JBG) => {
+//   console.log(JBG);
+// })
+// ..............................................................................
+// let utterance = new SpeechSynthesisUtterance("Hello world!");
+// speechSynthesis.speak(utterance);
+// ..............................................................................
+// let memberList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+// function gratings(members) {
+//   let utterance = new SpeechSynthesisUtterance(`welcome ${members}`);
+//   speechSynthesis.speak(utterance);
+// }
+// for (let i = 0; i < memberList.length; i++) {
+//   gratings(memberList[i]);
+// }
+// ..............................................................................
+// let memberList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
+// function md(members) {
+//   let utterance = new SpeechSynthesisUtterance(`welcome ${members} how are you?`);
+//   speechSynthesis.speak(utterance);
+// }
+// for (let i = 0; i < memberList.length; i++) {
+//   md(memberList[i]);
+// }
+// ..............................................................................
+// // let question = "What is your age?"
+// let question = "What is your name?"
+
+// // if (question == "What is your name?") {
+// //   console.log("my name is khairul hasan");
+  
+// // }
+
+// function Qanswer() {
+//   if (question == "What is your name?") {
+//     let utterance = new SpeechSynthesisUtterance(`My name is khairul hasan`);
+//     speechSynthesis.speak(utterance);
+//   }
+//   else if (question == "What is your age?") {
+//     let utterance = new SpeechSynthesisUtterance(`My age is 25`);
+//     speechSynthesis.speak(utterance);
+//   }
+// }
+// Qanswer()
+// ..............................................................................
+// let age = 20
+// function Qanswer() {
+//   if (age >=18) {
+//     let utterance = new SpeechSynthesisUtterance(`You can marry`);
+//     speechSynthesis.speak(utterance);
+//   }
+//   else if (question == "What is your age?") {
+//     let utterance = new SpeechSynthesisUtterance(`My age is 25`);
+//     speechSynthesis.speak(utterance);
+//   }
+// }
+// Qanswer()
+// ..............................................................................
+// js code for speechSynthesis End Start
+// (function() {
+//     'use strict';
+//     let utterance = new SpeechSynthesisUtterance("Hello world!");
+//     speechSynthesis.speak(utterance);
+// })();
+// js code for speechSynthesis End Codes 
+// ..............................................................................
